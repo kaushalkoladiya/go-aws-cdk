93ca9bc45ce2bf58f77849776749c5b8f8edd3b6
Kaushal <kaushalkoladiya123@gmail.com>
2024-10-13 17:48:48 -0400

    feat: implement user registration and login API with DynamoDB integration

--- lambda/api/api.go
+++ lambda/api/api.go
@@ -1,41 +1,117 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"lambda-func/database"
 	"lambda-func/types"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
 )
 
 type ApiHandler struct {
-	dbStore database.DynamoDBClient
+	dbStore database.UserStore
 }
 
-func NewApiHandler(dbStore database.DynamoDBClient) ApiHandler {
+func NewApiHandler(dbStore database.UserStore) ApiHandler {
 	return ApiHandler{
 		dbStore: dbStore,
 	}
 }
 
-func (api ApiHandler) RegisterUserHandler(event types.RegisterUser) error {
-	if event.Username == "" || event.Password == "" {
-		return fmt.Errorf("request has empty parameters")
+func (api ApiHandler) RegisterUserHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var registerUser types.RegisterUser
+
+	err := json.Unmarshal([]byte(request.Body), &registerUser)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid Request",
+			StatusCode: http.StatusBadRequest,
+		}, err
+	}
+
+	if registerUser.Username == "" || registerUser.Password == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid Request - empty parameters",
+			StatusCode: http.StatusBadRequest,
+		}, err
 	}
 
 	// does a user exists
-	userExists, err := api.dbStore.DoesUserExists(event.Username)
+	userExists, err := api.dbStore.DoesUserExists(registerUser.Username)
 	if err != nil {
-		return fmt.Errorf("there is an error checking if user exists %w", err)
+		return events.APIGatewayProxyResponse{
+			Body:       "Internal error",
+			StatusCode: http.StatusInternalServerError,
+		}, err
 	}
 
 	if userExists {
-		return fmt.Errorf("a user already exists with this username.")
+		return events.APIGatewayProxyResponse{
+			Body:       "a user already exists with this username",
+			StatusCode: http.StatusConflict,
+		}, nil
+	}
+
+	user, err := types.NewUser(registerUser)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Internal server error, please try again",
+			StatusCode: http.StatusInternalServerError,
+		}, fmt.Errorf("could not create new user - %w", err)
 	}
 
 	// insert user
-	err = api.dbStore.InsertUser(event)
+	err = api.dbStore.InsertUser(user)
 	if err != nil {
-		return fmt.Errorf("error registering the user %w", err)
+		return events.APIGatewayProxyResponse{
+			Body:       "internal server error",
+			StatusCode: http.StatusInternalServerError,
+		}, fmt.Errorf("Error inserting user - %w", err)
 	}
 
-	return nil
+	return events.APIGatewayProxyResponse{
+		Body:       "user inserted succesfully",
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func (api ApiHandler) LoginUserHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	type LoginRequest struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	var loginRequest LoginRequest
+
+	err := json.Unmarshal([]byte(request.Body), &loginRequest)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid Request",
+			StatusCode: http.StatusBadRequest,
+		}, err
+	}
+
+	user, err := api.dbStore.GetUser(loginRequest.Username)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Internal server error",
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+
+	if !types.ValidatePassword(user.PasswordHash, loginRequest.Password) {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid username or password",
+			StatusCode: http.StatusInternalServerError,
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       "Successfully login",
+		StatusCode: http.StatusOK,
+	}, nil
 }
--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,16 +1,24 @@
 package database
 
 import (
+	"fmt"
 	"lambda-func/types"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 const (
 	TABLE_NAME = "userTable"
 )
+
+type UserStore interface {
+	DoesUserExists(username string) (bool, error)
+	InsertUser(user types.User) error
+	GetUser(username string) (types.User, error)
+}
 
 type DynamoDBClient struct {
 	databaseStore *dynamodb.DynamoDB
@@ -47,7 +55,7 @@ func (u DynamoDBClient) DoesUserExists(username string) (bool, error) {
 	return true, nil
 }
 
-func (u DynamoDBClient) InsertUser(user types.RegisterUser) error {
+func (u DynamoDBClient) InsertUser(user types.User) error {
 	// assamble the item
 	item := &dynamodb.PutItemInput{
 		TableName: aws.String(TABLE_NAME),
@@ -56,7 +64,7 @@ func (u DynamoDBClient) InsertUser(user types.RegisterUser) error {
 				S: aws.String(user.Username),
 			},
 			"password": {
-				S: aws.String(user.Password),
+				S: aws.String(user.PasswordHash),
 			},
 		},
 	}
@@ -68,4 +76,33 @@ func (u DynamoDBClient) InsertUser(user types.RegisterUser) error {
 	}
 
 	return nil
+}
+
+func (u DynamoDBClient) GetUser(username string) (types.User, error) {
+	var user types.User
+
+	result, err := u.databaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(username),
+			},
+		},
+	})
+
+	if err != nil {
+		return user, err
+	}
+
+	if result.Item == nil {
+		return user, fmt.Errorf("user not found")
+	}
+
+	err = dynamodbattribute.UnmarshalMap(result.Item, &user)
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
 }
